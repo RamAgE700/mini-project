1e56461e22065ce2cfa83ea2f45783a0c6e9b5bf
RamAgE700 <adarshtomyasv@gmail.com>
2021-09-10 20:25:24 +0530

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    
+    <title>FORGEPC</title>
+</head>
+<body>
+<header>
+
+  
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">FORGEPC</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse " id="navbarNav">
+        <ul class="navbar-nav nav-fill">
+          <li class="nav-item nav-fill">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item nav-fill">
+            <a class="nav-link" href="#">category</a>
+          </li>
+          <li class="nav-item nav-fill">
+            <a class="nav-link" href="#">shop</a>
+          </li>
+          <li class="nav-item nav-fill">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">cart</a>
+          </li>
+
+          <i class="fas fa-user"></i>
+        </ul>
+       
+      </div>
+    </div>
+  </nav>
+  
+</header>
+</body>
+</html>
